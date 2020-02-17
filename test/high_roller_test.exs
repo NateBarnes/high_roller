61940b563fee62d2caee769bdb564e2bc9e0cf3f
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-02-17 12:33:52 -0800

    Added another die type to the test just to be sure

--- test/high_roller_test.exs
+++ test/high_roller_test.exs
@@ -4,12 +4,21 @@ defmodule HighRollerTest do
 
   describe "when rolling dice via method call" do
     test "rolls a d6 properly" do
-      result = HighRoller.roll(10_000, 6)
+      result = HighRoller.roll(100_000, 6)
                |> Enum.sum
-               |> Kernel./(10_000)
+               |> Kernel./(100_000)
                |> Float.round(1)
 
       assert result == 3.5
+    end
+
+    test "rolls a d20 properly" do
+      result = HighRoller.roll(100_000, 20)
+               |> Enum.sum
+               |> Kernel./(100_000)
+               |> Float.round(1)
+
+      assert result == 10.5
     end
   end
 end
