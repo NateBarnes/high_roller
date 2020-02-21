28b67f6157ec5b2dff7cc7d6d2111d95e54164ab
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-02-20 21:17:19 -0800

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HighRoller.MixProject do
   def project do
     [
       app: :high_roller,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
