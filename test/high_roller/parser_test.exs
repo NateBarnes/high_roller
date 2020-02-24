8cd8bf7d4b60215cfcd8588bce686ff057aa1848
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-02-23 17:32:02 -0800

    Added tests for operator parsing

--- test/high_roller/parser_test.exs
+++ test/high_roller/parser_test.exs
@@ -11,5 +11,18 @@ defmodule HighRoller.ParserTest do
 
       assert HighRoller.Parser.parse("3d20") == 32
     end
+
+    test "it should add specific dice together" do
+      HighRoller.RandomMock
+      |> stub(:roll, fn 2, 8 ->
+           [4, 8]
+         end)
+      HighRoller.RandomMock
+      |> stub(:roll, fn 2, 6 ->
+           [2, 4]
+         end)
+
+      assert HighRoller.Parser.parse("2d8+2d6") == 18
+    end
   end
 end
